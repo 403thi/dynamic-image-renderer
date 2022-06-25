c05981acd9715700e2021153d6900346b87f93c8
Rodrigo <96685294+rodrigojose01@users.noreply.github.com>
2022-06-24 21:22:58 -0300

    New Route and setup public folder

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
-import { Router } from "express"
+import { Router, Request, Response } from "express"
 import ImageController from "./controllers/ImageController"
 
 const router = Router()
+
+router.get('/', (req: Request, res: Response) => {
+    res.set('content-type', 'text/html')
+    res.sendFile(__dirname+'/public/demo.html')
+})
 
 router.get('/api', ImageController.index)
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+import path from "path"
+import express from "express"
 import app from '../api';
 
 const PORT = process.env.PORT || 3333
 
+app.use(express.static(path.join(__dirname,'public')))
+
 app.listen(PORT, () => {
     console.log('running in http://localhost:' + PORT)
 })
+
