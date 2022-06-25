54c33382716666bc7aed4da159d0756232b6d8f6
Rodrigo <96685294+rodrigojose01@users.noreply.github.com>
2022-06-24 21:51:57 -0300

    Changes

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ import app from '../api';
 
 const PORT = process.env.PORT || 3333
 
-app.use(express.static(path.join(__dirname,'public')))
-
 app.listen(PORT, () => {
     console.log('running in http://localhost:' + PORT)
 })
